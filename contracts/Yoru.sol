bc09297b197997a74fc13412102c040f25ad7f16
Charles Jhong <charlesjhongc@gmail.com>
2023-04-15 14:05:35 +0800

    upgrade Yoru to support ERC721

--- contracts/Yoru.sol
+++ contracts/Yoru.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.8.17;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { IYoru } from "./interfaces/IYoru.sol";
@@ -12,23 +13,35 @@ contract Yoru is IYoru {
         address payable _recipient,
         bytes32 _pkx, // ephemeral public key x coordinate
         bytes32 _ciphertext
-    ) external payable {
+    ) external payable override {
         require(msg.value > 0, "zero ETH value");
         emit Announcement(_recipient, msg.value, ETH_TOKEN_PLACHOLDER, _pkx, _ciphertext);
 
         _recipient.transfer(msg.value);
     }
 
-    function sendToken(
+    function sendERC20(
         address _recipient,
         address _tokenAddr,
         uint256 _amount,
         bytes32 _pkx, // ephemeral public key x coordinate
         bytes32 _ciphertext
-    ) external {
+    ) external override {
         require(_amount > 0, "zero amount");
         emit Announcement(_recipient, _amount, _tokenAddr, _pkx, _ciphertext);
 
         SafeERC20.safeTransferFrom(IERC20(_tokenAddr), msg.sender, _recipient, _amount);
+    }
+
+    function sendERC721(
+        address _recipient,
+        address _tokenAddr,
+        uint256 _tokenId,
+        bytes32 _pkx, // ephemeral public key x coordinate
+        bytes32 _ciphertext
+    ) external override {
+        emit Announcement(_recipient, _tokenId, _tokenAddr, _pkx, _ciphertext);
+
+        IERC721(_tokenAddr).transferFrom(msg.sender, _recipient, _tokenId);
     }
 }
