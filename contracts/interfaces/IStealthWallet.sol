bda218b6eef9fbb0239f7caacfa8452f4198da82
Charles Jhong <charlesjhongc@gmail.com>
2023-04-15 13:48:38 +0800

    remove unused and duplicated

--- contracts/interfaces/IStealthWallet.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.17;
-
-import { UserOperation } from "@account-abstraction/core/BaseAccount.sol";
-
-interface IStealthWallet {
-    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds) external returns (uint256 validationData);
-}
